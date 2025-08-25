fa9a683efafe956560204cd473f76ac5d646c8c1
minyii <minniieeerat@gmail.com>
2025-08-25 18:17:14 +0800

    images

--- warmbites/tutorial.swift
+++ warmbites/tutorial.swift
@@ -9,18 +9,111 @@
 import SwiftUI
 
 struct TutorialView: View {
+    @State private var navigateToGame = false
     var body: some View {
-        
-        
-         ZStack{
-             Image("bg")
-                 .resizable()
-                 .edgesIgnoringSafeArea(.all)
-         }
-        
+        NavigationStack {
+            ZStack{
+                Image("bg2")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .onTapGesture {
+                        navigateToGame = true
+                        print("MY is a bum")
+                    }
+                
+                NavigationLink(destination: textView(), isActive: $navigateToGame) {
+                    EmptyView()
+                }
+            }
+        }
     }
 }
-        
-        #Preview {
-         TutorialView()
+
+struct textView: View {
+    @State private var navigateToGame = false
+    var body: some View {
+        ZStack {
+            Image("bg2")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Text("This is the order station.")
+                    .frame(width: 300, height: 60, alignment: .center)
+                    .background(Color.beige)
+                    .foregroundStyle(Color.black)
+                    .font(.title2)
+                    .cornerRadius(10)
+            }
+                .onTapGesture {
+                    navigateToGame = true
+                    print("MY is a bigger bum")
+                }
+            
+            NavigationLink(destination: textView2(), isActive: $navigateToGame) {
+                EmptyView()
+            }
         }
+    }
+}
+
+struct textView2: View {
+    @State private var navigateToGame = false
+    var body: some View {
+        ZStack {
+            Image("bg2")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Text("We serve 3 dishes: noodles burgers and salads")
+                    .frame(width: 300, height: 100, alignment: .center)
+                    .background(Color.beige)
+                    .foregroundStyle(Color.black)
+                    .font(.title2)
+                    .cornerRadius(10)
+            }
+                .onTapGesture {
+                    navigateToGame = true
+                    print("MY is an even bigger bum")
+                }
+            
+            NavigationLink(destination: textView3(), isActive: $navigateToGame) {
+                EmptyView()
+            }
+        }
+    }
+}
+
+struct textView3: View {
+    @State private var navigateToGame = false
+    var body: some View {
+        ZStack {
+            Image("noodlegame")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Text("Pull the noodles and avoid the obstacles")
+                    .frame(width: 300, height: 100, alignment: .center)
+                    .background(Color.beige)
+                    .foregroundStyle(Color.black)
+                    .font(.title2)
+                    .cornerRadius(10)
+            }
+                .onTapGesture {
+                    navigateToGame = true
+                    print("MY is an even bigger bum")
+                }
+            
+            NavigationLink(destination: textView3(), isActive: $navigateToGame) {
+                EmptyView()
+            }
+        }
+    }
+}
+
+        
+#Preview {
+    TutorialView()
+}
